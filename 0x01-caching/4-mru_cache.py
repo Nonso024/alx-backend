4d7f720d03af27cc92a40b919f2409a167197cb8
Nonso024 <josephani772@gmail.com>
2024-01-03 14:12:02 +0100

    fix error

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class MRUCache(BaseCaching):
-    """ this class implements the LRU cache algorithm """
+    """ this class implements the MRU cache algorithm """
 
     def __init__(self):
         """ initializes class instance """
@@ -19,7 +19,7 @@ class MRUCache(BaseCaching):
         if key and item:
             self.cache_data[key] = item
             if len(self.cache_data) > self.MAX_ITEMS:
-                discard = min(self.cache_queue, key=self.cache.get)
+                discard = max(self.cache_queue, key=self.cache_queue.get)
                 del self.cache_data[discard]
                 del self.cache_queue[discard]
                 print("DISCARD: {}".format(discard))
